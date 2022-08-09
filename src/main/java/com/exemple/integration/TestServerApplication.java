790836acd5c41244077a0217a341c7541f75730f
doudouchat <baraultb@hotmail.com>
2022-08-09 21:56:43 +0100

    remove embedded kafka

--- src/main/java/com/exemple/integration/TestServerApplication.java
+++ src/main/java/com/exemple/integration/TestServerApplication.java
@@ -1,7 +1,5 @@
 package com.exemple.integration;
 
-import java.util.UUID;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,13 +8,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.test.EmbeddedKafkaBroker;
 
 import com.hazelcast.config.Config;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
-
-import kafka.server.KafkaConfig;
 
 @SpringBootApplication
 public class TestServerApplication {
@@ -43,38 +38,6 @@ public class TestServerApplication {
             LOG.info("STARTING EMBEDDED HAZELCAST");
 
             return Hazelcast.newHazelcastInstance(config);
-        }
-
-    }
-
-    @Configuration
-    @ConditionalOnProperty(value = { "port", "dir", "defaultTopic" }, prefix = "test.kafka")
-    public class KafkaConfiguration {
-
-        private final int kafkaPort;
-
-        private final String logDir;
-
-        private final String defaultTopic;
-
-        public KafkaConfiguration(@Value("${test.kafka.port}") int kafkaPort, @Value("${test.kafka.dir}") String logDir,
-                @Value("${test.kafka.defaultTopic}") String defaultTopic) {
-            this.kafkaPort = kafkaPort;
-            this.logDir = logDir;
-            this.defaultTopic = defaultTopic;
-
-        }
-
-        @Bean(destroyMethod = "destroy")
-        public EmbeddedKafkaBroker embeddedKafka() {
-
-            EmbeddedKafkaBroker embeddedKafka = new EmbeddedKafkaBroker(1, false, defaultTopic).brokerProperty(KafkaConfig.LogDirsProp(),
-                    logDir + "/" + UUID.randomUUID());
-            embeddedKafka.kafkaPorts(kafkaPort);
-
-            LOG.info("STARTING EMBEDDED KAFKA");
-
-            return embeddedKafka;
         }
 
     }
