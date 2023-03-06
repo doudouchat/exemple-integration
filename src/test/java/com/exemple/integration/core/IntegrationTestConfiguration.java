5694bfd46bdf34fc7d4aa5c803ed8512d7b287b1
doudouchat <baraultb@hotmail.com>
2023-03-06 19:46:52 +0000

    introduce ConfigurationProperties

--- src/test/java/com/exemple/integration/core/IntegrationTestConfiguration.java
+++ src/test/java/com/exemple/integration/core/IntegrationTestConfiguration.java
@@ -1,12 +1,17 @@
 package com.exemple.integration.core;
 
-import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
+import java.util.Collection;
+import java.util.List;
+
+import org.apache.kafka.clients.consumer.ConsumerRebalanceListener;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.TopicPartition;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.core.io.ClassPathResource;
 
 import com.exemple.authorization.core.client.resource.AuthorizationClientResourceConfiguration;
 import com.exemple.service.application.core.ApplicationConfiguration;
@@ -14,6 +19,8 @@ import com.exemple.service.resource.core.ResourceConfiguration;
 import com.hazelcast.client.HazelcastClient;
 import com.hazelcast.client.config.ClientConfig;
 import com.hazelcast.core.HazelcastInstance;
+
+import jakarta.annotation.PostConstruct;
 
 @Configuration
 @Import({ ResourceConfiguration.class, ApplicationConfiguration.class, AuthorizationClientResourceConfiguration.class })
@@ -23,23 +30,52 @@ public class IntegrationTestConfiguration {
     public static final String AUTHORIZATION_URL = System.getProperty("authorization.host", "http://localhost") + ":"
             + System.getProperty("authorization.port", "8090") + "/" + System.getProperty("authorization.contextpath", "ExempleAuthorization");
 
-    @Bean
-    public static PropertySourcesPlaceholderConfigurer propertyPlaceholderConfigurer() {
+    @Value("${event.topic}")
+    private String topic;
 
-        PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
+    @Autowired
+    private KafkaConsumer<?, ?> consumerEvent;
 
-        YamlPropertiesFactoryBean properties = new YamlPropertiesFactoryBean();
-        properties.setResources(new ClassPathResource("exemple-test.yml"));
-
-        propertySourcesPlaceholderConfigurer.setProperties(properties.getObject());
-        return propertySourcesPlaceholderConfigurer;
-    }
+    @Autowired
+    private KafkaConsumer<?, ?> consumerNewPassword;
 
     @Bean
     public HazelcastInstance hazelcastInstance() {
 
         ClientConfig config = ClientConfig.load();
         return HazelcastClient.newHazelcastClient(config);
+    }
+
+    @PostConstruct
+    public void suscribeConsumerEvent() throws Exception {
+
+        consumerEvent.subscribe(List.of(topic), new ConsumerRebalanceListener() {
+
+            @Override
+            public void onPartitionsRevoked(Collection<TopicPartition> partitions) {
+            }
+
+            @Override
+            public void onPartitionsAssigned(Collection<TopicPartition> partitions) {
+                consumerEvent.seekToBeginning(partitions);
+            }
+        });
+    }
+
+    @PostConstruct
+    public void suscribeConsumerNewPassword() throws Exception {
+
+        consumerNewPassword.subscribe(List.of("new_password"), new ConsumerRebalanceListener() {
+
+            @Override
+            public void onPartitionsRevoked(Collection<TopicPartition> partitions) {
+            }
+
+            @Override
+            public void onPartitionsAssigned(Collection<TopicPartition> partitions) {
+                consumerNewPassword.seekToBeginning(partitions);
+            }
+        });
     }
 
 }
