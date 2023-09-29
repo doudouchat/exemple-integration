ace3b2d56a525c48ad8786ec6813c2e014a1a228
doudouchat <baraultb@hotmail.com>
2023-09-29 19:33:31 +0100

    migration to java 21

--- src/test/java/com/exemple/integration/core/InitCassandra.java
+++ src/test/java/com/exemple/integration/core/InitCassandra.java
@@ -1,7 +1,7 @@
 package com.exemple.integration.core;
 
 import java.io.File;
-import java.nio.charset.StandardCharsets;
+import java.nio.charset.Charset;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Value;
@@ -37,7 +37,7 @@ public class InitCassandra {
 
     @SneakyThrows
     private static String[] splitScript(Resource script) {
-        return script.getContentAsString(StandardCharsets.UTF_8).trim().split(";");
+        return script.getContentAsString(Charset.defaultCharset()).trim().split(";");
     }
 
 }
